e93770c97dbf0032e44dda48583c48c826a4a1b8
Austin Olijar <73905380+AutiOmni@users.noreply.github.com>
2021-08-20 23:07:24 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 $('#appointment').click(function() {
+    $(this).toggleClass('active-link')
+// BRINGS UP CURRENT SECTION
     $('.appointment').toggleClass('active-sect');
+// REMOVES OTHER SECTION ACTIVES
     $('.gallery').removeClass('active-sect');
     $('.about').removeClass('active-sect');
+    $('#gallery').removeClass('active-link');
+    $('#about').removeClass('active-link');
 });
 
 $('#gallery').click(function() {
+    $(this).toggleClass('active-link')
+// BRINGS UP CURRENT SECTION
     $('.gallery').toggleClass('active-sect');
+// REMOVES OTHER SECTION ACTIVES
     $('.appointment').removeClass('active-sect');
-    $('.about').removeClass('active-sect');   
+    $('.about').removeClass('active-sect'); 
+    $('#appointment').removeClass('active-link');
+    $('#about').removeClass('active-link'); 
+      
 });
 
 $('#about').click(function() {
+    $(this).toggleClass('active-link')
+// BRINGS UP CURRENT SECTION
     $('.about').toggleClass('active-sect');
+// REMOVES OTHER SECTION ACTIVES
     $('.appointment').removeClass('active-sect');
     $('.gallery').removeClass('active-sect');
+    $('#appointment').removeClass('active-link');
+    $('#gallery').removeClass('active-link');
 });
 
