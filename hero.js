633bb841727c4dc1a73e7fd3e87667226033aeef
Austin Olijar <73905380+AutiOmni@users.noreply.github.com>
2021-09-12 13:38:51 -0500

    Add files via upload

--- hero.js
+++ hero.js
@@ -12,20 +12,28 @@ setTimeout(() => {
     $('.hero-one').css('transform', 'translate(100%, 0%)')
     $('.hero-two').css('transform', 'translate(-100%, 0%)')
 
-}, 2000);
+}, 3000);
 
 setTimeout(() => {
-     // PUT IMAGES BACK IN PLACE FOR NEXT ROTATION
-    $('.hero-nine').css('opacity', '1')
-    $('.hero-ten').css('opacity', '1')
 
+
+    $('.hero-eleven').css({'opacity': '0', 'transform': 'translate(-100%, 0%)'})
+    $('.hero-twelve').css({'opacity': '0', 'transform': 'translate(100%, 0%)'})
+    
     $('.hero-three').css('transform', 'translate(0%, 0%)')
     $('.hero-four').css('transform', 'translate(0%, 0%)')
-}, 2000);
+}, 3000);
 
 setTimeout(() => {
     $('.hero-three').css('transform', 'translate(0%, -100%)')
     $('.hero-four').css('transform', 'translate(0%, -100%)')
+
+    // PUT IMAGES BACK IN PLACE FOR NEXT ROTATION
+    $('.hero-nine').css('opacity', '1')
+    $('.hero-ten').css('opacity', '1')
+
+    $('.hero-eleven').css('opacity', '1')
+    $('.hero-twelve').css('opacity', '1')
 
     // PUT IMAGES BACK IN PLACE FOR NEXT ROTATION
     $('.hero-one').css({'opacity': '0', 'transform': 'translate(0%, 100%)'})
@@ -33,7 +41,7 @@ setTimeout(() => {
 
     $('.hero-five').css('transform', 'translate(0%, 0%)')
     $('.hero-six').css('transform', 'translate(0%, 0%)')
-}, 5000);
+}, 6000);
 
 
 setTimeout(() => {
@@ -49,7 +57,7 @@ setTimeout(() => {
 
     $('.hero-seven').css('transform', 'translate(0%, 0%)')
     $('.hero-eight').css('transform', 'translate(0%, 0%)')
-}, 8000);
+}, 9000);
 
 setTimeout(() => {
     $('.hero-seven').css('transform', 'translate(0%, 100%)')
@@ -64,11 +72,11 @@ setTimeout(() => {
 
     $('.hero-nine').css('transform', 'translate(0%, 0%)')
     $('.hero-ten').css('transform', 'translate(0%, 0%)')
-}, 11000);
+}, 12000);
 
 setTimeout(() => {
-    $('.hero-nine').css('transform', 'translate(0%, -100%)')
-    $('.hero-ten').css('transform', 'translate(0%, -100%)')
+    $('.hero-nine').css('transform', 'translate(100%, 0%)')
+    $('.hero-ten').css('transform', 'translate(-100%, 0%)')
 
         // PUT IMAGES BACK IN PLACE FOR NEXT ROTATION
         $('.hero-five').css('opacity', '1')
@@ -77,12 +85,27 @@ setTimeout(() => {
         $('.hero-seven').css({'opacity': '0', 'transform': 'translate(100%, 0%)'})
         $('.hero-eight').css({'opacity': '0', 'transform': 'translate(-100%, 0%)'})
 
+        $('.hero-eleven').css('transform', 'translate(0%, 0%)')
+        $('.hero-twelve').css('transform', 'translate(0%, 0%)')
+}, 15000);
+
+setTimeout(() => {
+    $('.hero-eleven').css('transform', 'translate(0%, -100%)')
+    $('.hero-twelve').css('transform', 'translate(0%, -100%)')
+
+        // PUT IMAGES BACK IN PLACE FOR NEXT ROTATION
+        $('.hero-nine').css({'opacity': '0', 'transform': 'translate(0%, -100%)'})
+        $('.hero-ten').css({'opacity': '0', 'transform': 'translate(0%, -100%)'})
+
+        $('.hero-seven').css('opacity', '1')
+        $('.hero-eight').css('opacity', '1')
+
         $('.hero-one').css('transform', 'translate(0%, 0%)')
         $('.hero-two').css('transform', 'translate(0%, 0%)')
-}, 14000);
+}, 18000);
 
 }
 
 heroSlider()
 
-setInterval(heroSlider, 14000)
+setInterval(heroSlider, 18000)
