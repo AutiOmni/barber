65faaaed86ebf9075566b795eb57ea49f4f708d2
Austin Olijar <73905380+OmniAuti@users.noreply.github.com>
2021-10-17 16:16:48 -0500

    Add files via upload

---
+++ appointment.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $firstName = $_POST['first-name'];
+    $lastName = $_POST['last-name'];
+    $phone = $_POST['phone-number'];
+    $phone = $_POST['phone-number'];
+
+
+    $mailTo = "austin.olijar@gmail.com"
+
+    mail();
+}
+
+?>
